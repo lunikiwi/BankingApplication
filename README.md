4f4b127af012c0547d195c95b98b81d9460d59e4
Tim <lunikiwi@proton.me>
2024-06-26 11:41:46 +0200

    Branch changes

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # BankingApplication
-Test
+
+## Overview
+
+This is a personal learning project. It's based on the following video:
+
+- https://www.youtube.com/watch?v=wQbEH4tVMJA&pp=ygUgamF2YSBiYW5raW5nIGFwcGxpY2F0aW9uIHByb2plY3Q%3D
+
+After this, I appended the project with several things:
+
+- GUI (incomplete)
+- user database (tbd)
+- transactions between users (tbd)
---
+++ UserServices.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class UserServices{
+    public static void loadUserCredentials() {
+        File file = new File("C:\\Develop\\Eigenes");
+        try {
+            Scanner fileScanner = new Scanner(file);
+            while (fileScanner.hasNextLine()) {
+                String[] credentials = fileScanner.nextLine().split(";");
+                User user = new User(credentials[0], credentials[1], new BankAccount()); // Assuming BankAccount() has a default constructor
+                addUser(user);
+            }
+            fileScanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("User credentials file not found.");
+            e.printStackTrace();
+        }
+
+    }
+    //zum Testen: Unit-Test schreiben
+    public static void writeUserToFile(User user) {
+        String dirName = "C:\\Develop\\Eigenes";
+        try {
+            BufferedWriter fileWriter = new BufferedWriter(new FileWriter(dirName + "\\userInformation.txt", true));
+            fileWriter.write(user.getUsername() + "," + user.getPassword());
+            fileWriter.newLine();
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void addUser(User user) {
+        put(user.getUsername(), user); //Add User to Map with username as key
+    }
+
+}
